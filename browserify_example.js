dbd6a8b2567048509d1ea075e050161bfdc945c0
Jack Dempsey <jdempsey@undertone.com>
2014-05-20 10:51:51 -0700

    simpler example of using browserify

--- browserify_example.js
+++ browserify_example.js
@@ -1,5 +1,12 @@
 var unique = require('uniq');
+var something = require('./something');
 
 var data = [1, 2, 2, 3, 4, 5, 5, 5, 6];
 
 console.log(unique(data));
+
+//console.log(something.Something.hello());
+
+console.dir(something);
+
+console.log(something.hello());
